068f7580e64077b975c960702528a690ac0d8433
Jason McCay <jason.mccay@gmail.com>
2014-11-19 22:08:17 -0600

    initial file ... with constants

---
+++ rover.rb
@@ -0,0 +1,27 @@
+
+
+# Rover coordination program
+# Jason McCay
+
+# Set up constants (for now)
+
+MIN_COORDINATES = [0,0]
+MAX_COORDINATES = [5,5]
+
+ROVER_ONE_LOCATION = [1,2]
+ROVER_ONE_STARTING_DIRECTION = "N"
+ROVER_ONE_MOVES = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+
+ROVER_TWO_STARTING_LOCATION = [3,3]
+ROVER_TWO_STARTING_DIRECTION = "E"
+ROVER_TWO_MOVES = ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
+
+
+# L makes rover spin 90 degrees to the left.
+# R makes rover spin 90 degrees to the right.
+# M makes rover move forward one spot for given heading
+
+
+
+
+
