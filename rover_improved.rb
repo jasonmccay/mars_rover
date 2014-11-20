c158934256a5e0b7879f24f07327fd504363379a
Jason McCay <jason.mccay@gmail.com>
2014-11-20 00:37:55 -0600

    initial attempt to classify rover program

---
+++ rover_improved.rb
@@ -0,0 +1,147 @@
+
+
+# (Improved) Rover Program
+# Jason McCay
+
+# Set up constants (for now)
+
+MIN_COORDINATES = [0,0]
+MAX_COORDINATES = [5,5]
+
+ROVER_ONE_STARTING_LOCATION = [1,2]
+ROVER_ONE_STARTING_X_COORDINATE = 1
+ROVER_ONE_STARTING_Y_COORDINATE = 2
+ROVER_ONE_STARTING_DIRECTION = "N"
+ROVER_ONE_MOVES = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
+
+ROVER_TWO_STARTING_LOCATION = [3,3]
+ROVER_TWO_STARTING_X_COORDINATE = 3
+ROVER_TWO_STARTING_Y_COORDINATE = 3
+ROVER_TWO_STARTING_DIRECTION = "E"
+ROVER_TWO_MOVES = ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
+
+
+# L makes rover spin 90 degrees to the left.
+# R makes rover spin 90 degrees to the right.
+# M makes rover move forward one spot for given heading
+
+
+class MoveMarsRover
+
+	def initialize(start_x_coordinate, start_y_coordinate, start_direction, commands)
+
+		@x_coord = start_x_coordinate
+		@y_coord = start_y_coordinate
+		@commands = commands
+		@start_direction = start_direction
+		@direction = 0
+
+	end
+
+	def convert_direction_to_degrees
+
+		if @start_direction == "N"
+			@direction = 0
+		elsif @start_direction == "E"
+			@direction = 90
+		elsif @start_direction == "S"
+			@direction = 180
+		elsif @start_direction == "W"
+			@direction = 270
+		end
+
+	end
+
+	def convert_degrees_to_direction
+
+		if @direction = 0
+			return "N"
+		end
+
+		if @direction = 90
+			return "E"
+		end
+
+		if @direction = 180
+			return "S"
+		end
+
+		if @direction = 270
+			return "W"
+		end
+	end
+
+
+	def turn_left
+
+		if @direction == 0
+			@direction = 360
+		end
+
+		@direction = @direction - 90
+
+	end
+
+	def turn_right
+
+		if @direction == 360
+			@direction = 0
+		end
+
+		@direction = @direction + 90
+
+	end
+
+	def move_forward
+
+		if @direction == 0 || @direction == 360
+			@y_coord = @y_coord + 1
+			puts "moved north"
+		end
+		
+		if @direction == 90
+			@x_coord = @x_coord + 1
+			puts "moved east"
+		end
+		
+		if @direction == 180
+			@y_coord = @y_coord - 1
+			puts "moved south"
+		end
+
+		if @direction == 270
+			@x_coord = @x_coord - 1
+			puts "moved west"
+		end
+
+	end
+
+	def process_commands
+
+		i = 0
+		
+		for i in 0..@commands.length
+		
+			if @command[i] = "L"
+				turn_left
+			end
+
+			if @command[i] = "R"
+				turn_right
+			end
+
+			if @command[i] = "M"
+				move_forward
+			end
+
+		end 
+
+	end
+
+
+	def output_final_location
+		print x_coord + " " + y_coord + " " + convert_degrees_to_direction
+	end
+
+
+end
