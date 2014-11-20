3074266f838f6ce57412fed4587c29c092e7d9fd
Jason McCay <jason.mccay@gmail.com>
2014-11-19 23:57:38 -0600

    figured out a bad if statement was causing bug

--- rover.rb
+++ rover.rb
@@ -9,24 +9,21 @@ MIN_COORDINATES = [0,0]
 MAX_COORDINATES = [5,5]
 
 ROVER_ONE_STARTING_LOCATION = [1,2]
+ROVER_ONE_STARTING_X_COORDINATE = 1
+ROVER_ONE_STARTING_Y_COORDINATE = 2
 ROVER_ONE_STARTING_DIRECTION = "N"
 ROVER_ONE_MOVES = ["L", "M", "L", "M", "L", "M", "L", "M", "M"]
 
 ROVER_TWO_STARTING_LOCATION = [3,3]
+ROVER_TWO_STARTING_X_COORDINATE = 3
+ROVER_TWO_STARTING_Y_COORDINATE = 3
 ROVER_TWO_STARTING_DIRECTION = "E"
 ROVER_TWO_MOVES = ["M", "M", "R", "M", "M", "R", "M", "R", "R", "M"]
-
-
-#Compass ... counter-clockwise ... north, west, south, east.
-
-COMPASS = [0, -1, -2, -3]
-
 
 
 # L makes rover spin 90 degrees to the left.
 # R makes rover spin 90 degrees to the right.
 # M makes rover move forward one spot for given heading
-
 
 
 ## define rover 1 initial direction
@@ -45,10 +42,14 @@ end
 
 location = ROVER_ONE_STARTING_LOCATION
 
+## define rover 1 inital coordinates
+x_loc = ROVER_ONE_STARTING_X_COORDINATE
+y_loc = ROVER_ONE_STARTING_Y_COORDINATE
+
 
 puts "initial direction -> #{direction}"
-puts "initial location[0] -> #{location[0]}"
-puts "initial location[1] -> #{location[1]}"
+puts "initial location[0] -> #{x_loc}."
+puts "initial location[1] -> #{y_loc}."
 
 for i in 0..ROVER_ONE_MOVES.length
 
@@ -83,27 +84,31 @@ for i in 0..ROVER_ONE_MOVES.length
 		puts "Moving!"
 		puts "My direction is: #{direction}"
 
-		if direction == 0 || 360
-			location[1] = location[1] + 1
+		if direction == 0 || direction == 360
+			y_loc = y_loc + 1
+			puts "moved north"
 		end
 		
 		if direction == 90
-			location[0] = location[0] + 1
+			x_loc = x_loc + 1
+			puts "moved east"
 		end
 		
 		if direction == 180
-			location[1] = location[1] - 1
+			y_loc = y_loc - 1
+			puts "moved south"
 		end
 
 		if direction == 270
-			location[0] = location[0] - 1
+			x_loc = x_loc - 1
+			puts "moved west"
 		end
 
 	end
 
 	puts "Finished command"
 	puts direction
-	puts location
+	puts "[#{x_loc},#{y_loc}]"
 
 end
 
